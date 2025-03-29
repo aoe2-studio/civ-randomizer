c6e57860d433d8307ab971fdb360636899632bb9
with-heart <with.heart+git@pm.me>
2025-03-29 14:33:20 -0400

    add spacing to roster

--- src/components/roster.tsx
+++ src/components/roster.tsx
@@ -1,12 +1,12 @@
-import { CIVS } from '@/constants'
+import { CIV_ICON_SIZE, CIVS } from '@/constants'
 import { ContextAwareCivIcon } from './civ-icon'
 
 export const Roster = () => {
   return (
-    <ul className="flex min-w-[100%] flex-wrap gap-1">
+    <ul className="flex min-w-[100%] flex-wrap gap-1 gap-y-2">
       {CIVS.map((civ) => (
         <li key={civ}>
-          <ContextAwareCivIcon civ={civ} />
+          <ContextAwareCivIcon civ={civ} size={CIV_ICON_SIZE / 2.5} />
         </li>
       ))}
     </ul>
