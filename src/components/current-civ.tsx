6750f731682f41763d630b9ef7be98397fa92f89
with-heart <with.heart+git@pm.me>
2025-03-31 12:34:50 -0400

    add played indicator to current civ #3

--- src/components/current-civ.tsx
+++ src/components/current-civ.tsx
@@ -1,17 +1,36 @@
 import { useCiv, useCurrentCiv } from '@/machines/app'
 import { capitalize } from '@/string'
 import type { Civ } from '@/types'
+import { AnimatePresence, motion } from 'motion/react'
 import { CivIcon } from './civ-icon'
 
 const CurrentCivInner = ({ civ }: { civ: Civ }) => {
-  const { togglePlayed } = useCiv(civ)
+  const { hasBeenPlayed, togglePlayed } = useCiv(civ)
 
   return (
     <div
       onClick={togglePlayed}
-      className="not-prose inline-flex h-[120px] w-[120px] flex-col items-center justify-center border-3 border-gray-400 shadow-2xl hover:border-gray-500"
+      className="not-prose inline-flex h-[200px] w-[200px] flex-col items-center justify-center gap-3 border-3 border-gray-400 text-2xl shadow-2xl hover:border-gray-500"
     >
-      <CivIcon civ={civ} />
+      <div className="relative">
+        <AnimatePresence>
+          {hasBeenPlayed ?
+            <motion.div
+              className="absolute inset-0 flex items-center justify-center"
+              variants={{
+                hidden: { opacity: 0, scale: 3 },
+                visible: { opacity: 1, scale: 1 },
+              }}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+            >
+              ✅
+            </motion.div>
+          : null}
+        </AnimatePresence>
+        <CivIcon civ={civ} size="lg" />
+      </div>
       <p>{capitalize(civ)}</p>
     </div>
   )
