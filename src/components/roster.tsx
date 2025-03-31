769c301b115ef236881f3ee42ec68cd08a208e1f
with-heart <with.heart+git@pm.me>
2025-03-31 10:28:12 -0400

    add enter/exit animations to roster icons

--- src/components/roster.tsx
+++ src/components/roster.tsx
@@ -1,30 +1,30 @@
-import { useCiv, useEnabledCivs } from '@/machines/app'
-import type { Civ } from '@/types'
-import { clsx } from 'clsx'
+import { useEnabledCivs } from '@/machines/app'
+import { AnimatePresence, motion } from 'motion/react'
 import { CivIcon } from './civ-icon'
-
-const RosterItem = ({ civ }: { civ: Civ }) => {
-  const { isEnabled, hasBeenPlayed } = useCiv(civ)
-  const className = clsx('transition-opacity', 'duration-200', {
-    'opacity-20': !isEnabled,
-    'opacity-50': isEnabled && hasBeenPlayed,
-  })
-
-  return <CivIcon className={className} civ={civ} size="md" />
-}
 
 export const Roster = () => {
   const enabledCivs = useEnabledCivs()
 
   return (
-    <ul className="not-prose flex min-w-[100%] flex-wrap gap-1 gap-y-2">
-      {enabledCivs.map((civ) => {
-        return (
-          <li key={civ}>
-            <RosterItem civ={civ} />
-          </li>
-        )
-      })}
-    </ul>
+    <motion.ul
+      layout
+      className="not-prose flex min-w-[100%] flex-wrap gap-1 gap-y-2"
+    >
+      <AnimatePresence>
+        {enabledCivs.map((civ) => {
+          return (
+            <motion.li
+              key={civ}
+              layout="position"
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.5 }}
+            >
+              <CivIcon civ={civ} />
+            </motion.li>
+          )
+        })}
+      </AnimatePresence>
+    </motion.ul>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { CurrentCiv } from '@/components/current-civ'
 import { Randomize } from '@/components/randomize'
 import { Roster } from '@/components/roster'
 import { AppProvider } from '@/machines/app'
+import { MotionConfig } from 'motion/react'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({
@@ -11,29 +12,31 @@ const inter = Inter({
 
 export default function Home() {
   return (
-    <AppProvider>
-      <div
-        className={`${inter.className} mx-auto prose min-h-screen w-screen max-w-6xl p-8 pb-20 antialiased sm:p-20 dark:prose-invert`}
-      >
-        <header>
-          <h1>aoe2 civ randomizer</h1>
-        </header>
+    <MotionConfig reducedMotion="user">
+      <AppProvider>
+        <div
+          className={`${inter.className} mx-auto prose min-h-screen w-screen max-w-6xl p-8 pb-20 antialiased sm:p-20 dark:prose-invert`}
+        >
+          <header>
+            <h1>aoe2 civ randomizer</h1>
+          </header>
 
-        <main className="flex flex-col gap-16">
-          <section>
-            <Roster />
-          </section>
+          <main className="flex flex-col gap-16">
+            <section>
+              <Roster />
+            </section>
 
-          <section className="flex flex-col items-center gap-12">
-            <CurrentCiv />
-            <Randomize />
-          </section>
+            <section className="flex flex-col items-center gap-12">
+              <CurrentCiv />
+              <Randomize />
+            </section>
 
-          <section>
-            <Configuration />
-          </section>
-        </main>
-      </div>
-    </AppProvider>
+            <section>
+              <Configuration />
+            </section>
+          </main>
+        </div>
+      </AppProvider>
+    </MotionConfig>
   )
 }
