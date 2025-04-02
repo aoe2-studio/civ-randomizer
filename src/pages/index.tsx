e4b0964475764a9fa96f3a17fde6d1925cf461f0
with-heart <with.heart+git@pm.me>
2025-04-02 10:49:09 -0400

    clean up index page components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,61 +3,45 @@ import { Configuration } from '@/components/configuration'
 import { CurrentCiv } from '@/components/current-civ'
 import { Randomize } from '@/components/randomize'
 import { Roster } from '@/components/roster'
+import { useIsClient } from '@/hooks'
 import { MotionConfig } from 'motion/react'
 import { Inter } from 'next/font/google'
-import { useEffect, useState } from 'react'
 
 const inter = Inter({
   subsets: ['latin'],
 })
 
-const Container = ({ children }: { children: React.ReactNode }) => {
+export default function Home() {
+  const isClient = useIsClient()
+
   return (
     <MotionConfig reducedMotion="user">
       <div
         className={`${inter.className} mx-auto prose min-h-screen w-screen max-w-6xl p-8 pb-20 antialiased sm:p-20 dark:prose-invert`}
       >
-        {children}
+        {isClient ?
+          <AppProvider>
+            <header>
+              <h1>aoe2 civ randomizer</h1>
+            </header>
+
+            <main className="flex flex-col gap-16">
+              <section>
+                <Roster />
+              </section>
+
+              <section className="flex flex-col items-center gap-12">
+                <CurrentCiv />
+                <Randomize />
+              </section>
+
+              <section>
+                <Configuration />
+              </section>
+            </main>
+          </AppProvider>
+        : <>Loading...</>}
       </div>
     </MotionConfig>
   )
-}
-
-const App = () => {
-  return (
-    <AppProvider>
-      <header>
-        <h1>aoe2 civ randomizer</h1>
-      </header>
-
-      <main className="flex flex-col gap-16">
-        <section>
-          <Roster />
-        </section>
-
-        <section className="flex flex-col items-center gap-12">
-          <CurrentCiv />
-          <Randomize />
-        </section>
-
-        <section>
-          <Configuration />
-        </section>
-      </main>
-    </AppProvider>
-  )
-}
-
-export default function Home() {
-  const [isClient, setIsClient] = useState(false)
-
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
-
-  return isClient ?
-      <Container>
-        <App />
-      </Container>
-    : <Container>Loading...</Container>
 }
